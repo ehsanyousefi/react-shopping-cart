6f8bd76acc6e10fa82e7e011cd203a05111de407
Ehsan Yousefi <ehsanyousefidev@gmail.com>
2020-08-28 18:41:08 +0430

    first commit

---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <h4>All Right is Reserved.</h4>
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <div>
+      <ul className="nav-links">
+        <li><a href="/">Shopping Cart</a></li>
+        <li><a href="/">Admin</a></li>
+      </ul>
+    </div>
+  )
+}
+
+export default Navbar
